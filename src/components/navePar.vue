6802fe7276502a80cf5aad027bf61c796cb03ad5
mahmdalqasm <mahmdalqasm12345@gmail.com>
2024-11-13 13:11:02 +0200

    some style to nave par

--- src/components/navePar.vue
+++ src/components/navePar.vue
@@ -13,3 +13,24 @@
 import { useRouter } from "vue-router";
 const router = useRouter();
 </script>
+
+<style scoped lang="scss">
+.nav ul {
+  margin: 0px 0px 20px;
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+  list-style: none;
+  padding: 20px 0px;
+  background-color: #078b69;
+  a {
+    color: white;
+    text-decoration: none;
+    font-size: 22px;
+    font-weight: bold;
+    &.router-link-active {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
