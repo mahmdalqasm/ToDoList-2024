1b68317a130ee90cdc437426f929c3688e3f67c6
mahmdalqasm <mahmdalqasm12345@gmail.com>
2024-11-13 12:37:39 +0200

    make form to entar the task

---
+++ src/views/HomePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1 style="margin-bottom: 20px">Add New Todos</h1>
+  <form action="" @submit.prevent>
+    <input
+      type="text"
+      v-model="todoObject.text"
+      placeholder="Enter Your todo"
+    />
+    <br />
+    <label
+      for=""
+      style="margin: 0px 10px 0px 0px; font-size: 18px; font-weight: bold"
+      >from</label
+    >
+    <input type="date" placeholder="from" v-model="todoObject.from" />
+    <label
+      for=""
+      style="margin: 0px 10px 0px 20px; font-size: 18px; font-weight: bold"
+      >to</label
+    >
+    <input type="date" placeholder="to" v-model="todoObject.to" />
+    <br />
+    <input type="submit" value="Add" />
+  </form>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const todoObject = ref({
+  text: "",
+  from: "",
+  to: "",
+  id: "",
+  createdAt: "",
+  isCompleted: "",
+});
+</script>
