eefe8a2906d2274815fd4200db2179f2a8f3de44
mahmdalqasm <mahmdalqasm12345@gmail.com>
2024-11-13 13:08:14 +0200

    create nave par

---
+++ src/components/navePar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="nav">
+    <ul>
+      <li v-for="route in router.options.routes" :key="route.name">
+        <router-link :to="{ name: route.name }"></router-link>
+        {{ route.meta.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+</script>
